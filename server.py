f8a33bfe9ff1dac9e2a77d72f8a2a5e12db76cc2
Vivek Selvarajan <vivek.selvarajan@gmail.com>
2020-03-22 17:03:27 +0530

    csv data saved to mongo collection

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
-from flask import *  
+from flask import *
+import pymongo
+import pandas as pd
+
 app = Flask(__name__)  
  
 @app.route('/')  
@@ -9,9 +12,26 @@ def upload():
 def success():  
     if request.method == 'POST':  
         print(request.files)
-        f = request.files['file']  
-        f.save(f.filename)  
-        return render_template("success.html", name = f.filename)  
+        f = request.files['file']
+        mongoClient = pymongo.MongoClient('localhost', 27017)
+        mongoDb = mongoClient['mphase']
+        collection = 'stock'
+        db = mongoDb[collection]
+        data = pd.read_csv(f)
+        data_json = json.loads(data.to_json(orient='records'))
+        names_key = {'% Deli. Qty to Traded Qty': '% Deli Qty to Traded Qty',
+                     'No. of Trades': 'No of Trades',
+                     'No.of Shares': 'No of Shares',
+                     'Total Turnover (Rs.)': 'Total Turnover (Rs)'
+                     }
+        for row in data_json:
+            for k, v in names_key.items():
+                for old_name in row:
+                     if k == old_name:
+                        row[v] = row.pop(old_name)
+        db.remove()
+        db.insert(data_json)
+        return render_template("success.html")
   
 if __name__ == '__main__':  
     app.run(debug = True)  
